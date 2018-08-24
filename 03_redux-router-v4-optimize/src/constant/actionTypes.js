b4ddaf827766bf6c00d10cc93bafe8b5d17c389a
QianLu(营销事业部_技术部) <qianlu@sogou-inc.com>
2018-08-24 15:53:41 +0800

    删除master内容

--- 03_redux-router-v4-optimize/src/constant/actionTypes.js
+++
@@ -1,9 +0,0 @@
-// ================ action types ================
-// thunk
-export const GET_USERS_SUCESS = 'GET_USERS_SUCESS';
-export const GET_USERS_FAIL = 'GET_USERS_FAIL';
-
-// saga
-export const GET_POSTS_SAGA = 'GET_POSTS_SAGA';
-export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
-export const GET_POSTS_FAIL = 'GET_POSTS_FAIL';
